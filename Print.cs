8986575ba1e37f68c58fa5095d4d243ef42dfd23
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2021-12-13 00:42:02 +0200

    refactoring

---
+++ Print.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Print
+    {
+        public static void Cells(char[] answer)
+        {
+            Console.Clear();
+
+            //array of numbers to numerate cells accoding to answer length
+            int[] numbers = new int[answer.Length];
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i + 1;
+
+            Text("\n");
+
+            foreach (var number in numbers)
+            {
+                if (number < 10 && number > 0) Text($"  {number} ");
+                else Text($" {number} ");
+            }
+            Text("\n");
+
+            for (int i = 0; i < answer.Length; i++)
+                Text(" ___", ConsoleColor.DarkBlue);
+            Text("\n");
+
+            for (int i = 0; i < answer.Length; i++)
+                Text("|   ", ConsoleColor.DarkBlue);
+            Text("|\n", ConsoleColor.DarkBlue);
+
+            foreach (var letter in answer)
+            {
+                Text($"|", ConsoleColor.DarkBlue);
+                Text($" {letter} ");
+            }
+            Text("|\n", ConsoleColor.DarkBlue);
+
+            for (int i = 0; i < answer.Length; i++)
+                Text("|___", ConsoleColor.DarkBlue);
+            Text("|\n\n", ConsoleColor.DarkBlue);
+        }
+
+        public static void HangedMan(int tries)
+        {
+            string[] hangedMan =
+            {            
+                "    ______   \n" +
+                "    |    |   \n" +
+                "    |    O   \n" +
+                "    |   /|\\ \n" +
+                "    |    |   \n" +
+                "    |   / \\ \n" +
+                "    |        \n" +
+                " ___|___",
+
+                "    ______   \n" +
+                "    |    |   \n" +
+                "    |    O   \n" +
+                "    |   /|\\ \n" +
+                "    |    |   \n" +
+                "    |   /    \n" +
+                "    |        \n" +
+                " ___|___",
+
+                "    ______   \n" +
+                "    |    |   \n" +
+                "    |    O   \n" +
+                "    |   /|\\ \n" +
+                "    |    |   \n" +
+                "    |        \n" +
+                "    |        \n" +
+                " ___|___",
+
+                "    ______   \n" +
+                "    |    |   \n" +
+                "    |    O   \n" +
+                "    |   /|   \n" +
+                "    |    |   \n" +
+                "    |        \n" +
+                "    |        \n" +
+                " ___|___",
+
+                "    ______   \n" +
+                "    |    |   \n" +
+                "    |    O   \n" +
+                "    |    |   \n" +
+                "    |    |   \n" +
+                "    |        \n" +
+                "    |        \n" +
+                " ___|___",
+
+                "    ______   \n" +
+                "    |    |   \n" +
+                "    |    O   \n" +
+                "    |        \n" +
+                "    |        \n" +
+                "    |        \n" +
+                "    |        \n" +
+                " ___|___"
+            };
+
+            Text(hangedMan[tries]);
+        }
+
+        public static void Text(string text, ConsoleColor color = ConsoleColor.Black)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,54 +1,52 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Threading;
 
 namespace Hangman
 {
     class Program
     {
+        private static Random random = new Random();
+
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Black;
 
-            Random random = new Random();
-
             //adding a file with words
-            string[] libraryOfWords = File.ReadAllLines(@"D:\Hangman\libraryOfWords.txt"); 
+            string[] libraryOfWords = File.ReadAllLines(@"C:\docs\code\Hangman\libraryOfWords.txt");
             string exitTheGame = string.Empty;
 
             while (exitTheGame != "n")
             {
                 string secretWord = string.Empty;
-                exitTheGame = string.Empty;
-                int humanOrAI = 0;
+                bool correctAnswer = false;
+                int humanOrAI = default;
                 int tries = 6;
 
                 while (humanOrAI != 1 && humanOrAI != 2)
                 {
                     Console.Clear();
-                    Print("[1] enter the word manually\n[2] generate word automatically\n\n");
-                    Print("make your choice: ");
+                    Print.Text(" [1] enter word manually\n" +
+                               " [2] generate word automatically\n\n");
+                    Print.Text(" make your choice: ");
+
                     int.TryParse(Console.ReadLine(), out humanOrAI);
-
-                    if (humanOrAI == 1)
-                    {
-                        Print("\nyou can enter a word in any language\n", ConsoleColor.DarkGreen);
-
-                        while (secretWord.Length <= 1 || secretWord.Length > 25)
-                        {
-                            Print("input (no more than 25 symbols): ");
-                            secretWord = Console.ReadLine().ToUpper();
-                        }
-                    }
-
-                    else if (humanOrAI == 2)
-                        secretWord = libraryOfWords[random.Next(0, libraryOfWords.Length - 1)].ToUpper();
-
-                    Console.Clear();
                 }
+
+                if (humanOrAI == 1)
+                {
+                    Print.Text("\n you can enter a word in any language\n", ConsoleColor.DarkGreen);
+
+                    while (secretWord.Length <= 1 || secretWord.Length > 25)
+                    {
+                        Print.Text(" input (no more than 25 symbols): ");
+                        secretWord = Console.ReadLine().ToUpper();
+                    }
+                }
+
+                else secretWord = libraryOfWords[random.Next(0, libraryOfWords.Length - 1)].ToUpper();
 
                 //initializing an array for pasting correct symbols
                 char[] answer = new char[secretWord.Length];
@@ -56,17 +54,17 @@ namespace Hangman
                     answer[i] = ' ';
 
                 //game will continue till the player has any tries left
-                while (tries != 0)
+                while (tries != 0 && !correctAnswer)
                 {
-                    bool charOrNot = false;
                     bool letterIsFound = false;
-                    char letter = ' ';
+                    bool charOrNot = false;
+                    char letter = default;
 
                     //searching for letter or digit
                     while (!charOrNot)
                     {
-                        PrintCells(answer);
-                        Print("Enter the letter: ");
+                        Print.Cells(answer);
+                        Print.Text(" Enter the letter: ");
                         charOrNot = char.TryParse(Console.ReadLine().ToUpper(), out letter);
                     }
 
@@ -84,164 +82,41 @@ namespace Hangman
                     {
                         tries--;
 
-                        if (tries == 5)
+                        if (tries != 0)
                         {
-                            Print($"Wrong, try again ({tries} tries left)\n");
+                            if (tries > 1)
+                                Print.Text($" Wrong, try again ({tries} tries left)\n");
 
-                            Print("    ______   \n" +
-                                  "    |    |   \n" +
-                                  "    |    O   \n" +
-                                  "    |        \n" +
-                                  "    |        \n" +
-                                  "    |        \n" +
-                                  "    |        \n" +
-                                  " ___|___");
+                            else if (tries == 1)
+                                Print.Text(" Wrong, last try left!\n");
 
-                            Thread.Sleep(4000);
+                            Print.HangedMan(tries);
+                            Thread.Sleep(3000);
                         }
 
-                        if (tries == 4)
+                        else
                         {
-                            Print($"Wrong, try again ({tries} tries left)\n");
-
-                            Print("    ______   \n" +
-                                  "    |    |   \n" +
-                                  "    |    O   \n" +
-                                  "    |    |   \n" +
-                                  "    |    |   \n" +
-                                  "    |        \n" +
-                                  "    |        \n" +
-                                  " ___|___");
-
-                            Thread.Sleep(4000);
-                        }
-
-                        if (tries == 3)
-                        {
-                            Print($"Wrong, try again ({tries} tries left)\n");
-
-                            Print("    ______   \n" +
-                                  "    |    |   \n" +
-                                  "    |    O   \n" +
-                                  "    |   /|   \n" +
-                                  "    |    |   \n" +
-                                  "    |        \n" +
-                                  "    |        \n" +
-                                  " ___|___");
-
-                            Thread.Sleep(4000);
-                        }
-
-                        if (tries == 2)
-                        {
-                            Print($"Wrong, try again ({tries} tries left)\n");
-
-                            Print("    ______   \n" +
-                                  "    |    |   \n" +
-                                  "    |    O   \n" +
-                                  "    |   /|\\ \n" +
-                                  "    |    |   \n" +
-                                  "    |        \n" +
-                                  "    |        \n" +
-                                  " ___|___");
-
-                            Thread.Sleep(4000);
-                        }
-
-                        if (tries == 1)
-                        {
-                            Print("Wrong, last try left!\n");
-
-                            Print("    ______   \n" +
-                                  "    |    |   \n" +
-                                  "    |    O   \n" +
-                                  "    |   /|\\ \n" +
-                                  "    |    |   \n" +
-                                  "    |   /    \n" +
-                                  "    |        \n" +
-                                  " ___|___");
-
-                            Thread.Sleep(4000);
-                        }
-
-                        if (tries == 0)
-                        {
-                            PrintCells(answer);
-
-                            Print("    ______   \n" +
-                                  "    |    |   \n" +
-                                  "    |    O   \n" +
-                                  "    |   /|\\ \n" +
-                                  "    |    |   \n" +
-                                  "    |   / \\ \n" +
-                                  "    |        \n" +
-                                  " ___|___");
-
-                            Print($"\n\nYou lose :( The secret word was: {secretWord}\n", ConsoleColor.Red);
-                            break;
+                            Print.Cells(answer);
+                            Print.HangedMan(tries);
+                            Print.Text($"\n\n You lose :( The secret word was: {secretWord}\n\n", ConsoleColor.DarkRed);
                         }
                     }
 
-                    string result = new string(answer);
-
-                    if (result == secretWord)
+                    correctAnswer = new string(answer) == secretWord;
+                    if (correctAnswer)
                     {
-                        PrintCells(answer);
-                        Print($"You win :) The secret word was: {secretWord}\n", ConsoleColor.DarkGreen);
-                        break;
+                        Print.Cells(answer);
+                        Print.Text($" You win :) The secret word was: {secretWord}\n\n", ConsoleColor.DarkGreen);
                     }
                 }
 
-                Print("\n");
-                while (exitTheGame != "n" && exitTheGame != "y")
+                do
                 {
-                    Print("Play again? [y] / [n]: ");
+                    Print.Text(" Play again? [y] / [n]: ");
                     exitTheGame = Console.ReadLine().ToLower();
                 }
+                while (exitTheGame != "n" && exitTheGame != "y");
             }
-        }
-        public static void PrintCells(char[] answer)
-        {
-            //array of numbers to numerate cells accoding to answer length
-            int[] numbers = new int[answer.Length];
-            for (int i = 0; i < numbers.Length; i++)
-                numbers[i] = i + 1;
-
-            Console.Clear();
-            Print("\n");
-
-            foreach (var number in numbers)
-            {
-                if (number < 10 && number > 0) Print($"  {number} ");
-                else Print($" {number} ");
-            }
-            Print("\n");
-
-            for (int i = 0; i < answer.Length; i++) 
-                Print(" ___", ConsoleColor.DarkBlue);
-            Print("\n");
-
-            for (int i = 0; i < answer.Length; i++) 
-                Print("|   ", ConsoleColor.DarkBlue);
-            Print("|\n", ConsoleColor.DarkBlue);
-
-            foreach (var letter in answer)
-            {
-                Print($"|", ConsoleColor.DarkBlue);
-                Print($" {letter} ");
-            }
-            Print("|\n", ConsoleColor.DarkBlue);
-
-            for (int i = 0; i < answer.Length; i++) 
-                Print("|___", ConsoleColor.DarkBlue);
-            Print("|\n\n", ConsoleColor.DarkBlue);
-        }
-
-        public static void Print(string text, ConsoleColor color = ConsoleColor.Black)
-        {
-            Console.ForegroundColor = color;
-            Console.Write(text);
-            Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }
